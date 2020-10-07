5413019d59c036bf1dc681cc94f86a085352926f
Nadilla Chantika Putri <nadillachantika@gmail.com>
2020-10-07 11:40:43 +0700

    menambahkan splashscreen animation

--- app/src/main/java/com/nadillla/tabunganapp/SplashScreenActivity.kt
+++ app/src/main/java/com/nadillla/tabunganapp/SplashScreenActivity.kt
@@ -4,27 +4,74 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.LinearLayout
 import com.nadillla.tabunganapp.Helper.SessionManager
 import com.nadillla.tabunganapp.Home.Home2Activity
 import com.nadillla.tabunganapp.View.MainActivity
+import com.wang.avi.AVLoadingIndicatorView
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreenActivity : AppCompatActivity() {
+
+
+    private val SPLASH_DELAY: Long = 3000 //3 seconds
+    private var mDelayHandler: Handler? = null
+    private var progressBarStatus = 0
+    var dummy: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
         val session = SessionManager(this)
+        mDelayHandler = Handler()
+        //Navigate with delay
+        mDelayHandler!!.postDelayed(mRunnable, SPLASH_DELAY)
 
-        Handler().postDelayed(Runnable {
+    }
 
-            if(session.login?:true){
-                startActivity(Intent(this,Home2Activity::class.java))
+    private fun launchMainActivity() {
+        var intent = Intent(this, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        startActivity(intent)
+        this.finish()
+        mDelayHandler!!.removeCallbacks(mRunnable)
 
-            }else{
-                startActivity(Intent(this,MainActivity::class.java))
+    }
+
+    private val mRunnable: Runnable = Runnable {
+
+        Thread(Runnable {
+            while (progressBarStatus < 100) {
+                // performing some dummy operation
+                try {
+                    dummy = dummy + 25
+                    Thread.sleep(100)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                // tracking progress
+                progressBarStatus = dummy
+
+                // Updating the progress bar
+                splash_screen_progress_bar.progress = progressBarStatus
             }
 
-            finish()
-        },2000)
+            //splash_screen_progress_bar.setProgress(10)
+
+            launchMainActivity()
+
+
+        }).start()
     }
+
+    override fun onDestroy() {
+
+        if (mDelayHandler != null) {
+            mDelayHandler!!.removeCallbacks(mRunnable)
+        }
+
+        super.onDestroy()
+    }
+
 }
